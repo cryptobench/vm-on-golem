fe7f9c1bc4608b8427d5e63684b9b7ec527311dc
Phillip Jensen <phillip@golemgrid.com>
2025-09-12 22:07:22 +0200

    UI

--- requestor-web/tailwind.config.ts
+++ requestor-web/tailwind.config.ts
@@ -9,6 +9,17 @@ const config: Config = {
     './node_modules/@tremor/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    borderRadius: {
+      none: '0',
+      sm: '0',
+      DEFAULT: '0',
+      md: '0',
+      lg: '0',
+      xl: '0',
+      '2xl': '0',
+      '3xl': '0',
+      full: '0',
+    },
     extend: {
       colors: {
         // Hetzner-like red palette
@@ -28,6 +39,7 @@ const config: Config = {
       boxShadow: {
         'tremor-card': '0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)',
       },
+      // Keep custom tokens for tremor but they will not be used where default rounded-* utilities are applied globally to 0
       borderRadius: {
         'tremor-small': '0.375rem',
         'tremor-default': '0.5rem',
